5c1a7b52959745d6b62e1b158a4f2cc414d0f9f9
Thauan Santos <nero.rbr@gmail.com>
2019-03-17 08:49:33 -0300

    melhorias e implementação das rotas de auth

--- start/routes/index.js
+++ start/routes/index.js
@@ -18,4 +18,8 @@ const Route = use('Route')
 
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
-})
+}).as('home')
+/**
+ * Importa as rotas de autenticação
+ */
+require('./auth')
