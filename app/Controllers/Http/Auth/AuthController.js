dc320f721a74e9ff45ab60d072dd1023936a98e4
Thauan Santos <nero.rbr@gmail.com>
2019-03-17 19:11:13 -0300

    logout method

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -45,7 +45,14 @@ class AuthController {
   }
 
   async logout({ request, response, auth }) {
-    //
+    let refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    await auth.authenticator('jwt').revokeTokens([refresh_token], true)
+    return response.status(204).send({})
   }
 
   async forgot({ request, response }) {
