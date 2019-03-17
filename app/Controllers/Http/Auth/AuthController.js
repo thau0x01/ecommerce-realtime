f4bcb0eb8b91c699295fddd114a3cec08c213e92
Thauan Santos <nero.rbr@gmail.com>
2019-03-17 18:58:23 -0300

    login and refresh token methods

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -23,11 +23,25 @@ class AuthController {
   }
 
   async login({ request, response, auth }) {
-    //
+    const { email, password } = request.all()
+
+    let data = await auth.withRefreshToken().attempt(email, password)
+
+    return response.send({ data })
   }
 
   async refresh({ request, response, auth }) {
-    //
+    var refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    const user = await auth
+      .newRefreshToken()
+      .generateForRefreshToken(refresh_token)
+
+    return response.send({ data: user })
   }
 
   async logout({ request, response, auth }) {
