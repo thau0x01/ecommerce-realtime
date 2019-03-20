1ed195b30ef9c4a3974018e6c9f39b698d1da755
Thauan Santos <nero.rbr@gmail.com>
2019-03-20 18:59:01 -0300

    Categories listing

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -3,7 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
-
+const Product = use('App/Models/Product')
 /**
  * Resourceful controller for interacting with products
  */
@@ -17,7 +17,15 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagiation }) {
+    const name = request.input('name')
+    const query = Product.query()
+    if (name) {
+      query.where('name', 'LIKE', `%${name}%`)
+    }
+    const products = await query.paginate(pagination.page, pagination.limit)
+    return response.send(products)
+  }
 
   /**
    * Create/save a new product.
