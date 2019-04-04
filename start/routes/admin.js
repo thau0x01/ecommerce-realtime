e4b6c36eeb293e785002c0263ee46eb3c34b0564
Thauan Santos <nero.rbr@gmail.com>
2019-04-04 03:28:03 -0300

    validators - parte 1

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -7,7 +7,14 @@ Route.group(() => {
   /**
    *  Categories resource routes
    */
-  Route.resource('categories', 'CategoryController').apiOnly()
+  Route.resource('categories', 'CategoryController')
+    .apiOnly()
+    .validator(
+      new Map([
+        [['categories.store'], ['Admin/StoreCategory']],
+        [['categories.update'], ['Admin/StoreCategory']]
+      ])
+    )
   /**
    * Products resource routes
    */
@@ -23,7 +30,9 @@ Route.group(() => {
    */
   Route.post('orders/:id/discount', 'OrderController.applyDiscount')
   Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
-  Route.resource('orders', 'OrderController').apiOnly()
+  Route.resource('orders', 'OrderController')
+    .apiOnly()
+    .validator(new Map([[['orders.store'], ['Admin/StoreOrder']]]))
 
   /**
    * Image Resource Routes
