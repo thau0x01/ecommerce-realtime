620333b9f6e192bc3593a81d52b1efadb56c6c3a
Thauan Santos <nero.rbr@gmail.com>
2019-04-04 02:55:08 -0300

    implementação de middlewares

--- start/routes/index.js
+++ start/routes/index.js
@@ -16,9 +16,6 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-}).as('home')
 /**
  * Importa as rotas de autenticação
  */
