96f6aad782fd2f295e9a01688d719c2afa07dd80
Thauan Santos <nero.rbr@gmail.com>
2019-04-05 01:53:00 -0300

    bugs em pedidos corrigidos

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -58,7 +58,8 @@ class OrderController {
         await service.syncItems(items)
       }
       await trx.commit()
-      order = await transform.item(order, Transformer)
+      order = await Order.find(order.id)
+      order = await transform.include('user,items').item(order, Transformer)
       return response.status(201).send(order)
     } catch (error) {
       await trx.rollback()
@@ -79,7 +80,9 @@ class OrderController {
    */
   async show({ params: { id }, response, transform }) {
     var order = await Order.findOrFail(id)
-    order = await transform.item(order, Transformer)
+    order = await transform
+      .include('items,user,discounts')
+      .item(order, Transformer)
     return response.send(order)
   }
 
@@ -101,7 +104,9 @@ class OrderController {
       await service.updateItems(items)
       await order.save(trx)
       await trx.commit()
-      order = await transform.item(order, Transformer)
+      order = await transform
+        .include('items,user,discounts,coupons')
+        .item(order, Transformer)
       return response.send(order)
     } catch (error) {
       await trx.rollback()
@@ -136,10 +141,10 @@ class OrderController {
     }
   }
 
-  async applyDiscount({ params: { id }, request, response }) {
+  async applyDiscount({ params: { id }, request, response, transform }) {
     const { code } = request.all()
     const coupon = await Coupon.findByOrFail('code', code.toUpperCase())
-    const order = await Order.findOrFail(id)
+    var order = await Order.findOrFail(id)
     var discount,
       info = {}
     try {
@@ -160,7 +165,9 @@ class OrderController {
         info.message = 'Não foi possível aplicar este cupom!'
         info.success = false
       }
-
+      order = await transform
+        .include('items,user,discounts,coupons')
+        .item(order, Transformer)
       return response.send({ order, info })
     } catch (error) {
       return response.status(400).send({ message: 'Erro ao aplicar o cupom!' })
